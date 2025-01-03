edcee1dac470c23b1e7a02bc42f7ca6a1b9b4457
CloudCraftWithFranck <yakengne@gmail.com>
2025-01-03 11:00:45 -0500

    Updating timezone nzme.

--- src/app/resources/content-i18n.js
+++ src/app/resources/content-i18n.js
@@ -9,7 +9,7 @@ const createI18nContent = (t) => {
         },
         role:      t("person.role"),
         avatar:    '/images/avatar.jpg',
-        location:  'Asia/Jakarta',        // Expecting the IANA time zone identifier, e.g., 'Europe/Vienna'
+        location:  'America/New_York',        // Expecting the IANA time zone identifier, e.g., 'Europe/Vienna'
         languages: ['English', 'Spanish', 'German']  // optional: Leave the array empty if you don't want to display languages
     }
 
