2cc808b2e0796d1c1fc8496af22f2c3446e92898
CloudCraftWithFranck <yakengne@gmail.com>
2025-01-03 21:44:24 -0500

    Adding consult section.

--- src/app/resources/renderContent.js
+++ src/app/resources/renderContent.js
@@ -1,6 +1,7 @@
 import { person, newsletter, social, home, about, blog, work, gallery } from './content';
 import { createI18nContent } from './content-i18n';
 import { i18n } from './config';
+import ConsultationPage from '../[locale]/consultation/page';
 
 const renderContent = (t) => {
     if ( i18n ) {
@@ -14,6 +15,7 @@ const renderContent = (t) => {
             about,
             blog,
             work,
+            consultation,
             gallery
         }
     }
