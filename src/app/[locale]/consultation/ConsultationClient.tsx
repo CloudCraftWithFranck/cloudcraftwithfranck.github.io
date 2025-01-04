1e43e7a42d849add7cfea9133564a369049e8f69
CloudCraftWithFranck <yakengne@gmail.com>
2025-01-03 22:35:24 -0500

    Banner consult section menu.

---
+++ src/app/[locale]/consultation/ConsultationClient.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import React from 'react';
+
+export default function ConsultationClient() {
+    return (
+        <div>
+            <button onClick={() => alert('Booking consultation...')}>
+                Book Now
+            </button>
+        </div>
+    );
+}
